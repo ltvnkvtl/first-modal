470ee76a1618ea4eb94e423f810ac580cedefdb5
ltvnkvtl <ltvnkvtl@users.noreply.github.com>
2020-05-31 07:12:23 +0300

    1st commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const modal = $.modal({
+  title: 'Litovkin Modal',
+  closable: true,
+  content: `
+    <p>Lorem ipsum dolor sit.</p>
+    <p>Lorem ipsum dolor sit.</p>
+  `,
+  width: '400px',
+  footerButtons: [
+    { 
+      text: 'Ok', 
+      type: 'primary', 
+      handler() { 
+        console.log('Primary btn clicked');
+        modal.close();
+      } 
+    },
+    { 
+      text: 'Cancel', 
+      type: 'danger', 
+      handler() { 
+        console.log('Danger btn clicked');
+        modal.close();
+      } 
+    }
+  ]
+});
